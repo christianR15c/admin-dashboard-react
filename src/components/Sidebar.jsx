0fc00c0306bc79ad5b6966e65544e1bd5f6fff6a
christian <habinezac15@gmail.com>
2022-06-22 17:50:04 +0200

    project folder structure setup

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { SiShopware } from 'react-icons/si';
+import { MdOutlineCancel } from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import { links } from '../data/dummy';
+
+const Sidebar = () => {
+  return <div className="ml-3"></div>;
+};
+
+export default Sidebar;
